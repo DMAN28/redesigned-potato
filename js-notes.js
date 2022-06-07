8c7a09a4fa8a973fcd7b1c8efcd9343088a1b79b
David Manriquez <dmanriqu@my.chemeketa.edu>
2022-06-06 22:34:01 -0700

    values and varibles

---
+++ js-notes.js
@@ -0,0 +1,32 @@
+////------------------------Values and variable------------------------\\\\
+
+// VAlUES piece of data most fundamental piece of information in programing
+
+// countryName is the VARIABLE the assigned VALUE is USA
+let countryName = "USA"
+
+let confederationName = "CONCACAF"
+
+let teamPlacing = "1"
+
+// Variable naming conventions first name lowercase second uppercase. Variable can not start with a number or an uppercase letter how ever it can contain numbers, letters, underscores or a dollar sign.
+
+
+////------------------------Data types --------------------------------------\\\\
+
+////------------------------ 7 primitive data types ------------------------\\\\
+
+///////////// NUMBER floating point number --> Used for integers/////////////
+let masterBuilder = 33
+///////////// STRING sequence of characters --> Used for text/////////////
+let vaultCode = "Planck's Constant";
+///////////// Boolean logical type that can only be true or false  --> Used for making decisions/////////////
+let correctCode = true;
+
+///////////// UNDEFINED value taken by a variable that is not yet defined ('empty value')/////////////
+
+///////////// NULL also stands for 'empty value'/////////////
+
+///////////// SYMBOL value that is unique and cannot be changed /////////////
+
+///////////// BITINT larger integer's than the number type can hold/////////////
